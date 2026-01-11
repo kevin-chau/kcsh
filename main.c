86a647205e3f8f97c80b7cd095d4746be57a342a
kevin-chau <kevin@kevinchau.com>
2026-01-11 03:17:32 -0800

    Basic looping working.

--- main.c
+++ main.c
@@ -6,11 +6,14 @@
 // Follow tutorial https://brennan.io/2015/01/16/write-a-shell-in-c/
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 #define KSH_READ_LINE_BUFFER_SIZE 1024
+#define KSH_TOKEN_BUFFER_SIZE 64
+#define KSH_TOKEN_DELIMITERS " \t\r\n\a"
 
 // Handle buffer allocation error
 void handle_buffer_allocation_error(char * buffer) {
@@ -56,6 +59,41 @@ char *ksh_read_line(void) {
 }
 
 
+// handle tokens buffer allocation error
+void handle_tokens_buffer_allocation_error(char** tokens) {
+    if (!tokens) {
+        fprintf(stderr, "ksh: memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Tokenizer
+char **ksh_split_line(char *line) {
+    int buffer_size = KSH_TOKEN_BUFFER_SIZE;
+    int position = 0;
+    char **tokens = malloc(buffer_size * sizeof(char*));
+    char *token;
+    handle_tokens_buffer_allocation_error(tokens);
+
+    token = strtok(line, KSH_TOKEN_DELIMITERS);
+    while (token) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= buffer_size) {
+            buffer_size += KSH_TOKEN_BUFFER_SIZE;
+            tokens = realloc(tokens, buffer_size * sizeof(char*));
+            handle_tokens_buffer_allocation_error(tokens);
+        }
+
+        // Get the next token
+        token = strtok(NULL, KSH_TOKEN_DELIMITERS);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+
 // Basic loop
 // - Read
 // - Parse
@@ -68,11 +106,12 @@ void ksh_loop(void) {
     do {
         printf("ksh$ ");
         line = ksh_read_line();
-        // args = ksh_split_line(line);
+        args = ksh_split_line(line);
         // status = ksh_execute(args);
+        status = 1;
 
         free(line);
-        // free(args);
+        free(args);
     } while (status);
 }
 
